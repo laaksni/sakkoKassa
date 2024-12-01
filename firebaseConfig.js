4f0f44f7944574052a1f0a51a61c967f0dda70ad
laaksni <niko.laaksonen@hotmail.com>
2024-12-02 01:01:11 +0200

    Api suojaus lisätty

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,14 +1,15 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { FIREBASE_API_KEY, FIREBASE_AUTH_DOMAIN, FIREBASE_PROJECT_ID, FIREBASE_STORAGE_BUCKET, FIREBASE_MESSAGING_SENDER_ID, FIREBASE_APP_ID } from "@env";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyA3glKRGTwXmdG_UgCUmcHC1M--AcO0uaY",
-    authDomain: "sakkokassakhk.firebaseapp.com",
-    projectId: "sakkokassakhk",
-    storageBucket: "sakkokassakhk.firebasestorage.app",
-    messagingSenderId: "355346603040",
-    appId: "1:355346603040:web:cf3bd7c9d23b63d50d7045"
-  };
+    apiKey: FIREBASE_API_KEY,
+    authDomain: FIREBASE_AUTH_DOMAIN,
+    projectId: FIREBASE_PROJECT_ID,
+    storageBucket: FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
+    appId: FIREBASE_APP_ID,
+};
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
